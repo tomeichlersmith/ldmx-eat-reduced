ccc980d9261d16bf1a2be0cf549acee29e2bf206
tomeichlersmith <eichl008@umn.edu>
2026-01-27 09:36:07 -0600

    fill cutflow histograms for a few differetn hcal options

--- ReducedEaT.cxx
+++ ReducedEaT.cxx
@@ -7,6 +7,39 @@
 
 #include <cmath>
 
+/// updated layer weights for v14 that adds the pre-ceeding
+/// odd-layer weight to the even layer weight
+static const std::vector<float> even_only_layer_weights = {
+  2.329,
+  -1, 4.339+6.495,
+  -1, 7.490+8.595,
+  -1, 10.253+10.915,
+  -1, 10.915+10.915,
+  -1, 10.915+10.915,
+  -1, 10.915+10.915,
+  -1, 10.915+10.915,
+  -1, 10.915+10.915,
+  -1, 10.915+10.915,
+  -1, 10.915+10.915,
+  -1, 10.915+10.915,
+  -1, 14.783+18.539,
+  -1, 18.539+18.539, 
+  -1, 18.539+18.539,
+  -1, 18.539+18.539,
+  -1, 18.539+18.539,
+  -1
+};
+/// for re-reconstructing energy of an ecal hit
+static const float mip_si_energy = 0.130; // MeV
+
+/// function to check if a ecal hit should be included
+/// only even layers from the first 32 layers
+bool include_ecal_hit(const ldmx::EcalHit& hit) {
+  ldmx::EcalID id(static_cast<unsigned int>(hit.getID()));
+  if (id.layer() >= 32) return false;
+  return (id.layer() % 2 == 0);
+}
+
 /// count number of quad-bars are needed to reach out to the input
 /// strip assuming we start filling from the center (strip 20)
 int hcal_hit_n_required_quads(int strip) {
@@ -17,10 +50,64 @@ int hcal_hit_cost(const ldmx::HcalHit& h) {
   return h.getLayer()*hcal_hit_n_required_quads(h.getStrip());
 }
 
+using HcalHitFilter = bool (*)(const ldmx::HcalHit& hit);
+
+/**
+ * Hcal "prototype" from CERN 2022 testbeam is
+ * - 9 layers each with 2 quads and 10 layers each with 3 quad bars
+ *
+ * One Hcal "module" is 8 layers with 5 quad bars (40 strips)
+ */
+bool is_in_back_hcal(const ldmx::HcalHit& hit) {
+  return (hit.getSection() == 0);
+}
+
+bool is_in_first_six_modules(const ldmx::HcalHit& hit) {
+  if (not is_in_back_hcal(hit)) return false;
+  return (hit.getLayer() < 6*8 + 1);
+}
+
+bool is_in_prototype_then_six_modules(const ldmx::HcalHit& hit) {
+  if (not is_in_back_hcal(hit)) return false;
+  if (hit.getLayer() < 9 + 1) {
+    // first 9 layers have 2 quads
+    return hcal_hit_n_required_quads(hit.getStrip()) < 3;
+  } else if (hit.getLayer() < 19 + 1) {
+    // 10-19 have 3 quads
+    return hcal_hit_n_required_quads(hit.getStrip()) < 4;
+  } else {
+    // next 6 modules (8 layers each) have all 5 quads
+    return hit.getLayer() < (19 + 6*8 + 1);
+  }
+}
+
+bool is_in_six_modules_then_reverse_prototype(const ldmx::HcalHit& hit) {
+  if (not is_in_back_hcal(hit)) return false;
+  if (hit.getLayer() < 6*8+1) {
+    // first 6 modules (8 layers each) have all 5 quads
+    return true;
+  } else if (hit.getLayer() < (6*8 + 10 + 1)) {
+    // next 10 layers have 3 quads
+    return hcal_hit_n_required_quads(hit.getStrip()) < 4;
+  } else if (hit.getLayer() < (6*8 + 10 + 9 + 1)) {
+    // next 9 layers have 2 quads
+    return hcal_hit_n_required_quads(hit.getStrip()) < 3;
+  } else {
+    return false;
+  }
+}
+
+static const std::map<std::string, HcalHitFilter> REDUCED_HCAL_OPTIONS = {
+  {"entireback", is_in_back_hcal},
+  {"sixonly", is_in_first_six_modules},
+  {"funnel", is_in_six_modules_then_reverse_prototype},
+  {"megaphone", is_in_prototype_then_six_modules}
+};
+
 class ReducedEaT : public framework::Analyzer {
   int max_pe_threshold = 10;
   float rms_event_size_threshold = 20; // mm
-  float reco_ecal_energy = 2760; // MeV
+  float low_energy_threshold = 3160; // MeV
  public:
   ReducedEaT(const std::string& name, framework::Process& p)
     : framework::Analyzer(name, p) {}
@@ -47,56 +134,29 @@ void ReducedEaT::onProcessStart() {
         "Strip", 40, -0.5, 39.5,
         "Layer", 100, 0.5, 100.5
     );
-    histograms_.create(
-        selection+"_hcalmaxpe",
-        "PE", 50, 0, 50
-    );
   }
 
-  for (const std::string& selection : {"trigger", "hcalmaxpe"}) {
-    histograms_.create(
-        selection+"_ecalrms",
-        "Ecal RMS [mm]", 50, 0, 50);
+  for (const auto& [hcal_name, _filter]: REDUCED_HCAL_OPTIONS) {
+    for (const std::string& selection : {"trigger", "ecalrms", "lowenergy"}) {
+      histograms_.create(
+          hcal_name+"_"+selection+"_hcalmaxpe",
+          "PE", 50, 0, 50
+      );
+    }
+
+    for (const std::string& selection : {"trigger", "hcalmaxpe"}) {
+      histograms_.create(
+          hcal_name+"_"+selection+"_ecalrms",
+          "Ecal RMS [mm]", 50, 0, 50);
+    }
+  
+    for (const std::string& selection: {"trigger", "hcalmaxpe", "ecalrms", "final"}) {
+      histograms_.create(
+          hcal_name+"_"+selection+"_total_ecal_rec_energy",
+          "Ecal Reco Energy [MeV]",
+          400,0,4000);
+    }
   }
-
-  for (const std::string& selection: {"trigger", "hcalmaxpe", "ecalrms", "final"}) {
-    histograms_.create(
-        selection+"_total_ecal_rec_energy",
-        "Ecal Reco Energy [MeV]",
-        400,0,4000);
-  }
-}
-
-static const std::vector<float> even_only_layer_weights = {
-  2.329,
-  -1, 4.339+6.495,
-  -1, 7.490+8.595,
-  -1, 10.253+10.915,
-  -1, 10.915+10.915,
-  -1, 10.915+10.915,
-  -1, 10.915+10.915,
-  -1, 10.915+10.915,
-  -1, 10.915+10.915,
-  -1, 10.915+10.915,
-  -1, 10.915+10.915,
-  -1, 10.915+10.915,
-  -1, 14.783+18.539,
-  -1, 18.539+18.539, 
-  -1, 18.539+18.539,
-  -1, 18.539+18.539,
-  -1, 18.539+18.539,
-  -1
-};
-static const float mip_si_energy = 0.130; // MeV
-
-bool include_ecal_hit(const ldmx::EcalHit& hit) {
-  ldmx::EcalID id(static_cast<unsigned int>(hit.getID()));
-  if (id.layer() >= 32) return false;
-  return (id.layer() % 2 == 0);
-}
-
-bool include_hcal_hit(const ldmx::HcalHit& hit) {
-  return (hit.getSection() == 0);
 }
 
 void ReducedEaT::analyze(const framework::Event& event) {
@@ -114,15 +174,6 @@ void ReducedEaT::analyze(const framework::Event& event) {
       ecal_hits.push_back(&ecal_hit);
     }
   }
-
-  const auto& all_hcal_hits{event.getCollection<ldmx::HcalHit>("HcalRecHits", "")};
-  std::vector<const ldmx::HcalHit*> hcal_hits;
-  for (const auto& hcal_hit: all_hcal_hits) {
-    if (include_hcal_hit(hcal_hit)) {
-      hcal_hits.push_back(&hcal_hit);
-    }
-  }
-
   float total_energy{0},
         center_x{0},
         center_y{0},
@@ -148,68 +199,96 @@ void ReducedEaT::analyze(const framework::Event& event) {
     shower_rms /= total_energy;
   }
 
+  const auto& all_hcal_hits{event.getCollection<ldmx::HcalHit>("HcalRecHits", "")};
+
+  /****************************************************************************
+   * Use Entire Back Hcal to find "Best" hit that vetos event
+   ***************************************************************************/
   int n_hcal_veto_hits{0};
   float hcal_max_pe{0};
   int hcal_hit_veto_cost{20*200};
   std::pair<int,int> min_quad_bar{40,100};
   const ldmx::HcalHit* min_cost_veto{nullptr}, *central_veto{nullptr};
-  for (const auto* hcal_hit: hcal_hits) {
-    if (hcal_hit->getPE() > hcal_max_pe) {
-      hcal_max_pe = hcal_hit->getPE();
+  for (const auto& hcal_hit: all_hcal_hits) {
+    if (not is_in_back_hcal(hcal_hit)) {
+      continue;
     }
-    if (hcal_hit->getPE() < max_pe_threshold) {
+    if (hcal_hit.getPE() > hcal_max_pe) {
+      hcal_max_pe = hcal_hit.getPE();
+    }
+    if (hcal_hit.getPE() < max_pe_threshold) {
       // this hit would not veto the event, skip it
       continue;
     }
     n_hcal_veto_hits++;
-    int cost{hcal_hit_cost(*hcal_hit)};
+    int cost{hcal_hit_cost(hcal_hit)};
     if (cost < hcal_hit_veto_cost) {
       hcal_hit_veto_cost = cost;
-      min_cost_veto = hcal_hit;
+      min_cost_veto = &hcal_hit;
     }
-    int n_req_quads{hcal_hit_n_required_quads(hcal_hit->getStrip())};
+    int n_req_quads{hcal_hit_n_required_quads(hcal_hit.getStrip())};
     if (n_req_quads < min_quad_bar.first or
-        (n_req_quads == min_quad_bar.first and hcal_hit->getLayer() < min_quad_bar.second)) {
-      min_quad_bar = {n_req_quads, hcal_hit->getLayer()};
-      central_veto = hcal_hit;
+        (n_req_quads == min_quad_bar.first and hcal_hit.getLayer() < min_quad_bar.second)) {
+      min_quad_bar = {n_req_quads, hcal_hit.getLayer()};
+      central_veto = &hcal_hit;
     }
   }
 
   histograms_.fill("n_hcal_veto_hits", n_hcal_veto_hits);
-  histograms_.fill("trigger_total_ecal_rec_energy", total_energy);
   histograms_.fill("trigger_hcal_min_cost_strip_layer",
       min_cost_veto ? min_cost_veto->getStrip() : -1,
       min_cost_veto ? min_cost_veto->getLayer() : -1);
   histograms_.fill("trigger_hcal_central_strip_layer",
       central_veto ? central_veto->getStrip() : -1,
       central_veto ? central_veto->getLayer() : -1);
-  histograms_.fill("trigger_ecalrms", shower_rms);
-  histograms_.fill("trigger_hcalmaxpe", hcal_max_pe);
-  if (hcal_max_pe < max_pe_threshold) {
-    histograms_.fill("hcalmaxpe_total_ecal_rec_energy", total_energy);
-    histograms_.fill("hcalmaxpe_ecalrms", shower_rms);
-    if (shower_rms < rms_event_size_threshold) {
-      histograms_.fill("final_total_ecal_rec_energy", total_energy);
-    }
-  }
-
   if (shower_rms < rms_event_size_threshold) {
-    histograms_.fill("ecalrms_total_ecal_rec_energy", total_energy);
     histograms_.fill("ecalrms_hcal_min_cost_strip_layer",
         min_cost_veto ? min_cost_veto->getStrip() : -1,
         min_cost_veto ? min_cost_veto->getLayer() : -1);
     histograms_.fill("ecalrms_hcal_central_strip_layer",
         central_veto ? central_veto->getStrip() : -1,
         central_veto ? central_veto->getLayer() : -1);
-    histograms_.fill("ecalrms_hcalmaxpe", hcal_max_pe);
-    if (total_energy < 3160) {
+    if (total_energy < low_energy_threshold) {
       histograms_.fill("lowenergy_hcal_min_cost_strip_layer",
         min_cost_veto ? min_cost_veto->getStrip() : -1,
         min_cost_veto ? min_cost_veto->getLayer() : -1);
       histograms_.fill("lowenergy_hcal_central_strip_layer",
         central_veto ? central_veto->getStrip() : -1,
         central_veto ? central_veto->getLayer() : -1);
-      histograms_.fill("lowenergy_hcalmaxpe", hcal_max_pe);
+    }
+  }
+
+  /****************************************************************************
+   * do event selection for a few different options of a reduced Hcal
+   ***************************************************************************/
+  for (const auto& [hcal_name, should_keep] : REDUCED_HCAL_OPTIONS) {
+    float hcal_max_pe{0};
+    for (const auto& hcal_hit : all_hcal_hits) {
+      if (not should_keep(hcal_hit)) {
+        continue;
+      }
+      if (hcal_hit.getPE() > hcal_max_pe) {
+        hcal_max_pe = hcal_hit.getPE();
+      }
+    }
+
+    histograms_.fill(hcal_name+"_trigger_total_ecal_rec_energy", total_energy);
+    histograms_.fill(hcal_name+"_trigger_ecalrms", shower_rms);
+    histograms_.fill(hcal_name+"_trigger_hcalmaxpe", hcal_max_pe);
+    if (hcal_max_pe < max_pe_threshold) {
+      histograms_.fill(hcal_name+"_hcalmaxpe_total_ecal_rec_energy", total_energy);
+      histograms_.fill(hcal_name+"_hcalmaxpe_ecalrms", shower_rms);
+      if (shower_rms < rms_event_size_threshold) {
+        histograms_.fill(hcal_name+"_final_total_ecal_rec_energy", total_energy);
+      }
+    }
+  
+    if (shower_rms < rms_event_size_threshold) {
+      histograms_.fill(hcal_name+"_ecalrms_total_ecal_rec_energy", total_energy);
+      histograms_.fill(hcal_name+"_ecalrms_hcalmaxpe", hcal_max_pe);
+      if (total_energy < low_energy_threshold) {
+        histograms_.fill(hcal_name+"_lowenergy_hcalmaxpe", hcal_max_pe);
+      }
     }
   }
 }
