ec780aa6320cf34dbcf22edfa1515698a1813a5f
tomeichlersmith <eichl008@umn.edu>
2026-01-30 09:15:10 -0600

    add another hcal option

--- final-ecal-rec-energy-plot.py
+++ final-ecal-rec-energy-plot.py
@@ -10,6 +10,7 @@ import hist
 
 hcal_options = {
     'entireback': 'Entire Back',
+    'prototype' : 'CERN 2022 Prototype',
     'sixonly': 'Only 6 Modules',
     'funnel' : '6 Modules then Prototype',
     'megaphone': 'Prototype then 6 Modules'
