2ec34b91b5fb58838471fad64aee81277f5fea01
tomeichlersmith <eichl008@umn.edu>
2026-03-12 10:25:23 -0500

    patch up cards and job list writing

--- reach/construct-datacard.py
+++ reach/construct-datacard.py
@@ -1,25 +1,40 @@
 from harvester.datacard import DataCard
 import json
 import os
-    
+import argparse
+from pathlib import Path
 
-with open('bkgd-prediction.json','r') as f:
-    bkgd_yield = json.load(f)
+parser = argparse.ArgumentParser()
+parser.add_argument('bkgd', type=Path, help='background predictions to do reach for')
+args = parser.parse_args()
 
-with open('sig-eff.json','r') as f:
-    sig_eff = json.load(f)
+here = Path(__file__).parent
 
+with open(here / 'paper-bkgd-prediction.json', 'r') as f:
+    paper_bkgd_yield = json.load(f)["8"]
 
-with open('jm-requests/jobs.list','w') as jobs:
-    for beam in [4, 8]:
-        by = bkgd_yield[beam]
-        for proc, se in seg_eff[beam].items():
-            datacard_path = str(output / f'{proc}.txt')
+with open(here / 'paper-sig-eff.json', 'r') as f:
+    paper_sig_eff = json.load(f)["8"]
+
+with open(args.bkgd, 'r') as f:
+    bkgd_yields = json.load(f)
+
+combine_dir = here / 'cards'
+combine_dir.mkdir(parents=True, exist_ok=True)
+
+with open(combine_dir / 'jobs.list', 'w') as jobs:
+    for option, by in [('paper',paper_bkgd_yield)]+list(bkgd_yields.items()):
+        for proc, se in paper_sig_eff.items():
+            datacard_path = str(combine_dir / f'{option}-{proc}.txt')
             (
                 DataCard(3)
-                .sig(*se.value)
-                .bkg(*by.value)
-                .bkg_stat(*((by.up - by.lo)/2/by.value))
+                .sig(*se)
+                .bkg(*by["fit_val"])
+                .bkg_stat(*(
+                    ((up - lo)/2/fit_val)
+                    for up, lo, fit_val in
+                    zip(by["up"], by["lo"], by["fit_val"], strict=True)
+                ))
                 .bkg_syst(0.05)
                 .bkg_syst(0.0, 0.6, 0.7)
                 .write(datacard_path)
@@ -27,7 +42,8 @@ with open('jm-requests/jobs.list','w') as jobs:
             for q in [0.025, 0.16, 0.5, 0.84, 0.975]:
                 jobs.write(' '.join([
                     datacard_path,
-                    f'--keyword-value option={name}',
+                    f'--keyword-value option={option}',
                     f'--mass {proc[2:]}',
                     f'--expectedFromGrid={q}'
-                ])
+                ]))
+                jobs.write('\n')
